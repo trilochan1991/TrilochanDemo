6181e99b033a680e9e4932e30aa7c2504d8d8866
trilochan1991 <trilochan.pradhan88@gmail.com>
2018-07-16 00:52:31 +0530

    fibonic

---
+++ src/Sample1/FibonicSeries.java
@@ -0,0 +1,22 @@
+package Sample1;
+
+public class FibonicSeries {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 1;
+		int k = 0;
+		System.out.print(a);
+		while(k <= 10){				
+			System.out.print(b);
+			k = a+b;
+			a = b;
+			b = k;			
+		}
+		
+		
+
+	}
+
+}
